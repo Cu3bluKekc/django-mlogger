0ef06ca0e95b6bdc73c77fadd460f10c71f8c9a1
Alexander <cu3blukekc@gmail.com>
2013-05-24 14:58:24 +0400

    add url reverse support in admin

--- mlogger/admin.py
+++ mlogger/admin.py
@@ -4,10 +4,34 @@
 
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
-
+from django.utils.safestring import mark_safe
+from django.core.urlresolvers import reverse
 from mlogger.models import Log
 
-class __Log(admin.ModelAdmin):
+
+class AdminLog(admin.ModelAdmin):
     list_display = ('user', 'action', 'content_type', 'timestamp')
-admin.site.register(Log, __Log)
+    readonly_fields = ('user', 'action', '_object', 'data', 'timestamp')
+    exclude = ('content_type', 'oid', )
+
+    def _object(self, object):
+        app_label = object.content_type.app_label
+        model = object.content_type.model
+        oid = object.oid
+        text = u'{app_label}.{model}:{oid}'.format(app_label=app_label,
+                                                   model=model,
+                                                   oid=oid)
+        url = u'#'
+        if object.action in ('create', 'update'):
+            try:
+                url = reverse('admin:{app_label}_{model}_change'.\
+                        format(app_label=app_label,
+                               model=model), args=[oid])
+            except:
+                pass
+        return u'<a href="{url}">{text}</a>'.format(url=url, text=text)
+    _object.allow_tags = True
+    _object.short_description = _('object')
+    
+admin.site.register(Log, AdminLog)
 Log.description = _(u'This model consists of all meaningful events of system.')
