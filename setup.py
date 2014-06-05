bac599d4a22f58b11d2e9e698e67b3e92af238a4
Alexandr Trubnikov <trubnikov@corp.sputnik.ru>
2014-06-05 16:56:00 +0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='mlogger',
-      version='0.1',
-      description='Application that helps your models to keep track of all their changes.',
-      author='Ruslan Popov',
-      author_email='ruslan.popov@gmail.com',
-      url='http://github.com/RaD/django-mlogger/tree/master',
-      packages=['mlogger',],
-      classifiers=['Development Status :: 4 - Beta',
-                   'Environment :: Web Environment',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Topic :: Utilities'],
-      )
+setup(
+    name='mlogger',
+    version='0.1',
+    description='Application that helps your models to keep track of all their changes.',
+    author='Ruslan Popov',
+    author_email='ruslan.popov@gmail.com',
+    url='http://github.com/RaD/django-mlogger/tree/master',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ],
+)
+
